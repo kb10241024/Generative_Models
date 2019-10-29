22d6ed0ba428a332ecce31b342245138a73d77b7
kb10241024 <43873939+kb10241024@users.noreply.github.com>
2019-10-29 15:42:21 +0530

    generate outputs after each epoch
    generate and save outputs generated for 16 random noise given as input after each training epoch

---
+++ GAN/DCGAN/generate_and_save_images.py
@@ -0,0 +1,14 @@
+def generate_and_save_images(model, epoch, test_input):
+  # Notice `training` is set to False.
+  # This is so all layers run in inference mode (batchnorm).
+  predictions = model(test_input, training=False)
+
+  fig = plt.figure(figsize=(4,4))
+
+  for i in range(predictions.shape[0]):
+      plt.subplot(4, 4, i+1)
+      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+      plt.axis('off')
+
+  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+  plt.show()
