8f9872758311a47e8932ff5bfeed3be4ea908bfe
kb10241024 <43873939+kb10241024@users.noreply.github.com>
2019-10-29 15:30:57 +0530

    generator loss function created
    The generator's loss quantifies how well it was able to trick the discriminator. Intuitively, if the generator is performing well, the discriminator will classify the fake images as real (or 1). Here, we will compare the discriminators decisions on the generated images to an array of 1s.

---
+++ GAN/DCGAN/generator_loss.py
@@ -0,0 +1,5 @@
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
